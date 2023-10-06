d524f0c56a30d54ab0547e3dd2020803ec627e63
Guillermo Casares <guillermocasares@outlook.com>
2023-10-06 10:47:20 -0500

    WIP: WebScrapping done getting the information. Printing in the console for now.

--- ScrappingResultadosEliminatorias/Program.cs
+++ ScrappingResultadosEliminatorias/Program.cs
@@ -15,7 +15,7 @@ class Program
             {
                 driver.Navigate().GoToUrl("https://www.google.com/search?q=conmebol+qualifiers&sca_esv=571003301&sxsrf=AM9HkKmJlo2qrTJJS8CX89-I36q7iOVkrg%3A1696528098923&source=hp&ei=4vYeZci4NICmqtsP96CGkAM&iflsig=AO6bgOgAAAAAZR8E8vB-h30eVfxphbSWdMuwAEXLZfZt&gs_ssp=eJzj4tZP1zc0MjLLzbNMMWD0Ek7Oz8tNTcrPUSgsTczJTMtMLSoGALEfC1A&oq=conmebol+q&gs_lp=Egdnd3Mtd2l6Igpjb25tZWJvbCBxKgIIATIIEAAYywEYgAQyBRAuGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARImjxQAFjpHXABeACQAQCYAYECoAGnD6oBBjAuMTAuMbgBA8gBAPgBAcICBBAjGCfCAgcQLhiKBRgnwgILEC4YgAQYxwEY0QPCAgcQIxiKBRgnwgIMECMYigUYExiABBgnwgIIEC4YywEYgATCAgoQLhjLARiABBgKwgIHEC4YgAQYCsICBxAAGIAEGAo&sclient=gws-wiz#sie=lg;/g/11t9_xctp4;2;/g/1226mn9d;mt;fp;1;;;");
 
-                Thread.Sleep(5000);
+                Thread.Sleep(500);
 
                 IWebElement visibleElement = driver.FindElement(By.XPath("//div[@id='liveresults-sports-immersive__updatable-league-matches']"));
 
@@ -50,21 +50,29 @@ class Program
 
                     foreach (HtmlNode partido in partidos)
                     {
-                        string equipo1 = partido.SelectSingleNode(".//div[@data-df-team-mid]:nth-child(2)").InnerText;
-                        string equipo2 = partido.SelectSingleNode(".//div[@data-df-team-mid]:nth-child(3)").InnerText;
+                        var nodesEquipos = partido.SelectNodes(".//td[contains(@class, 'tns-c')]/div/div[contains(@class, 'liveresults-sports-immersive__hide-element')]");
+                        string equipo1 = nodesEquipos[0].InnerText;
+                        string equipo2 = nodesEquipos[1].InnerText;
 
-                        string golesEquipo1 = partido.SelectSingleNode(".//td[@class='imspo_mt__tt-w'][1]/div").InnerText;
-                        string golesEquipo2 = partido.SelectSingleNode(".//td[@class='imspo_mt__tt-w'][2]/div").InnerText;
 
-                        string fechaPartido = partido.SelectSingleNode(".//div[@class='imspo_mt__cmd']").InnerText;
+                        var nodesBanderas = partido.SelectNodes(".//td[contains(@class, 'imspo_mt__lgc')]");
+                        const string protocolo = "https";
+                        string bandera1 = $"{protocolo}:{nodesBanderas[0].ChildNodes[0].Attributes["src"].Value}";
+                        string bandera2 = $"{protocolo}:{nodesBanderas[1].ChildNodes[0].Attributes["src"].Value}";
 
-                        Console.WriteLine("Jornada: " + jornadaTitle);
-                        Console.WriteLine("Equipo 1: " + equipo1);
-                        Console.WriteLine("Equipo 2: " + equipo2);
-                        Console.WriteLine("Goles Equipo 1: " + golesEquipo1);
-                        Console.WriteLine("Goles Equipo 2: " + golesEquipo2);
+                        var nodeGolesEquipos = partido.SelectNodes(".//td[contains(@class, 'tns-c')]/div/div[contains(@class, 'imspo_mt__tt-w')]"); 
+                        string golesEquipo1 = nodeGolesEquipos !=null ? nodeGolesEquipos[0].InnerText:" X ";
+                        string golesEquipo2 = nodeGolesEquipos != null ? nodeGolesEquipos[1].InnerText : " X ";
+
+                        var nodeFechaPartidoJugado = partido.SelectSingleNode(".//div[@class='imspo_mt__cmd']//span[last()]");
+                        var nodeFechaPartidoNoJugado = partido.SelectSingleNode(".//div[@class='imspo_mt__ns-pm-s']");
+                        string fechaPartido = nodeFechaPartidoJugado != null ? nodeFechaPartidoJugado.InnerText : $"{nodeFechaPartidoNoJugado.ChildNodes[0].InnerText} HORA: {nodeFechaPartidoNoJugado.ChildNodes[1].InnerText}";
+
+                        Console.WriteLine("Bandera 1: " + bandera1);
+                        Console.WriteLine("Bandera 2: " + bandera2);
+                        Console.WriteLine($"Marcador: {equipo1} {golesEquipo1} - {equipo2} {golesEquipo2} ");
                         Console.WriteLine("Fecha del Partido: " + fechaPartido);
-                        Console.WriteLine("-----------------------------------------");
+                        Console.WriteLine();
                     }
                 }
                 
