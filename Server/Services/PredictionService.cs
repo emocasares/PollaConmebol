0f14070a2cd72021c326e6546baaee28258507d8
Guillermo Casares <guillermocasares@outlook.com>
2023-10-13 12:54:18 -0500

    Calculate Leaderboard real implementation Fix layout to make it more visible in mobile

--- Server/Services/PredictionService.cs
+++ Server/Services/PredictionService.cs
@@ -5,6 +5,7 @@ using PollaEngendrilClientHosted.Shared;
 using PollaEngendrilClientHosted.Shared.Models.DTO;
 using PollaEngendrilClientHosted.Shared.Models.Entity;
 using PollaEngendrilClientHosted.Shared.Models.ViewModel;
+using System.Linq;
 
 namespace PollaEngendrilClientHosted.Server.Services
 {
@@ -23,14 +24,44 @@ namespace PollaEngendrilClientHosted.Server.Services
 
         public List<PlayerLeaderboardViewModel> CalculateLeaderboard()
         {
-
-            var leaderboard = new List<PlayerLeaderboardViewModel>
+            var actualMatches = dbContext.Matches.Where(m => m.HomeTeamScore.HasValue && m.AwayTeamScore.HasValue).ToList();
+            var predictions = dbContext.Predictions
+                .Where(p => dbContext.Matches
+                    .Any(m => m.Id == p.MatchId && m.HomeTeamScore.HasValue && m.AwayTeamScore.HasValue))
+                .ToList();
+            var users = dbContext.Users.ToList();
+            var userPoints = new Dictionary<Shared.Models.Entity.User, int>();
+            foreach (var prediction in predictions)
             {
-                new PlayerLeaderboardViewModel { Position = 1, Name = "Jugador 1", Score = 20 },
-                new PlayerLeaderboardViewModel { Position = 2, Name = "Jugador 2", Score = 18 },
-            };
+                var actualMatch = actualMatches.FirstOrDefault(m => m.Id == prediction.MatchId);
+                var actualResult = new MatchResult() { HomeTeamScore = actualMatch.HomeTeamScore.Value, AwayTeamScore = actualMatch.AwayTeamScore.Value };
+                var predictedResult = new PredictionRequestDTO
+                {
+                    HomeTeamScore = prediction.HomeTeamScore,
+                    AwayTeamScore = prediction.AwayTeamScore
+                };
 
-            return leaderboard;
+                var points = CalculatePoints(actualResult, predictedResult).Points;
+                var user = users.FirstOrDefault(u => u.Id == prediction.UserId);
+
+                if (userPoints.ContainsKey(user))
+                {
+                    userPoints[user] += points;
+                }
+                else
+                {
+                    userPoints[user] = points;
+                }
+            }
+
+            var sortedLeaderboard = userPoints.OrderByDescending(kvp => kvp.Value)
+                                              .Select((kvp, index) => new PlayerLeaderboardViewModel
+                                              {
+                                                    Position = index + 1,
+                                                    Name = kvp.Key.Email,
+                                                    Score = kvp.Value
+                                              }).ToList();
+            return sortedLeaderboard;
         }
 
         public PredictionResponseDTO CalculatePoints(MatchResult actualResult, PredictionRequestDTO predictedResult)
